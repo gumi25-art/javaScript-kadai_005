59ea649c262adc1ac7137225131724cba9cb2c26
gumi25-art <160398203+gumi25-art@users.noreply.github.com>
2024-06-13 11:57:01 +0800

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,18 +1,10 @@
 // 変数の宣言.値の代入
-let userName;'体重';
-let userNumber = 68;
-
-// 値の再代入
- userName = '身長';
-userNumber = 1.7;
-
-// 値の再代入
-userName = '計算式';
-userNumber = 68/1.7**2;
-
+let weight=68;
+let height=1.7;
+let BMI= weight/ (height*height);
 
 // コンソールへの出力
-console.log(68/1.7**2);
+console.log(BMI);
 
 
 
